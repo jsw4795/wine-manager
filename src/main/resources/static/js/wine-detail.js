058259a4eb26c5ccf7d8367e51a4d5caaf90a0a5
UK <jsw4795@naver.com>
2024-05-13 21:59:21 +0900

    wine-detail에서도 페이지, 스크롤위치 유지
    처음부터 데이터를 ajax로 가져오는 timeline과 달리
    첫 페이지는 이미 가져와져 있는 상태로 시작하기때문에 고민하느냐로 시간이 좀 걸렸다.

--- src/main/resources/static/js/wine-detail.js
+++ src/main/resources/static/js/wine-detail.js
@@ -1,65 +1,101 @@
-var historyPage = 1;
-var reviewPage = 1;
-var pageSize = 5;
-var wineId;
+var globalPageSize = 5;
+
+var wineDetailData = {
+	wineId: -1,
+	historyPage: 2,
+	reviewPage: 2,
+	scroll: 0,
+}
+
+window.onpageshow = function(event) {
+	// 뒤로가기로 온 경우
+	if (event.persisted || (window.performance && window.performance.navigation.type == 2 && sessionStorage.getItem("wineDetailData"))) {
+		console.log(JSON.parse(sessionStorage.getItem("wineDetailData")));
+		
+		// 저장된 와인 아이디와 같을 때만 로직 실행
+		if(JSON.parse(sessionStorage.getItem("wineDetailData")).wineId != $("#wine-id").text()) {
+			return;
+		}
+		
+		wineDetailData = JSON.parse(sessionStorage.getItem("wineDetailData"));
+		
+		let scroll = wineDetailData.scroll;
+		if(wineDetailData.historyPage > 2){
+			loadHistory(2, (wineDetailData.historyPage - 1) * globalPageSize, function() {
+				$(document).scrollTop(scroll);
+			})
+		}
+		if(wineDetailData.reviewPage > 2){
+			loadReview(2, (wineDetailData.reviewPage - 1) * globalPageSize, function() {
+				$(document).scrollTop(scroll);
+			})
+		}
+		
+	}
+}
 
 $(()=> {
-	wineId = $("#wine-id").text();
+	wineDetailData.wineId = $("#wine-id").text();
 	
 	$("#history-load-btn").on("click", function() {
-		historyPage++;
-		$(this).addClass("hidden");
-		loadHistory();
+		loadHistory(wineDetailData.historyPage, globalPageSize);
 	})
 	
 	$("#review-load-btn").on("click", function() {
-		reviewPage++;
-		$(this).addClass("hidden");
+		loadReview(wineDetailData.reviewPage, globalPageSize)
+	})
+	
+	// 페이지 나갈때 정보 저장
+	$(window).on("beforeunload", function() {
+		wineDetailData.historyPage = Math.max(2, wineDetailData.historyPage);
+		wineDetailData.reviewPage = Math.max(2, wineDetailData.reviewPage);
+		wineDetailData.scroll = $(document).scrollTop();
 		
+		sessionStorage.setItem("wineDetailData", JSON.stringify(wineDetailData));
 	})
 })
 
-function loadHistory() {
+function loadHistory(page, pageSize, callBack) {
+	$("#history-load-btn").addClass("hidden");
 	$.ajax({
 		url: "/wine/load",
 		type: "GET",
-		data: {wineId: wineId, page: historyPage, pageSize: pageSize, type: "log"},
+		data: {wineId: wineDetailData.wineId, page: page, pageSize: pageSize, type: "log"},
 		dataType: "HTML",
 		success: function(result) {
-			//let loadedNumber = $(result).find(".history").length;
+			wineDetailData.historyPage++;
 			
 			htmlForMobile = $(result).find("#wine-log-mobile").html();
 			htmlForPC = $(result).find("#wine-log-pc tbody").html();
 			
 			$("#history-mobile-container").append(htmlForMobile);
 			$("#history-pc-container").append(htmlForPC);
-			
-			// 꽉 채워서 가져왔으면 로드 버튼 생성
-			//if(loadedNumber == pageSize * 2) // 모바일과 pc버전 두개를 가져오기 때문
-				//$("#history-load-btn").removeClass("hidden");
 				
 			if($(result).find("#hasNext").text() == "true")
 				$("#history-load-btn").removeClass("hidden");
+				
+			if(callBack)
+				callBack();
 		}
 	});
 }
-function loadReview() {
+function loadReview(page, pageSize, callBack) {
+	$("#history-load-btn").addClass("hidden");
 	$.ajax({
 		url: "/wine/load",
 		type: "GET",
-		data: {wineId: wineId, page: historyPage, pageSize: pageSize, type: "review"},
+		data: {wineId: wineDetailData.wineId, page: page+1, pageSize: pageSize, type: "review"},
 		dataType: "HTML",
 		success: function(result) {
-			//let loadedNumber = $(result).find(".review").length;
+			wineDetailData.reviewPage++;
 			
 			$("#review-container").append($(result).find("#html-result").html());
-			
-			// 꽉 채워서 가져왔으면 로드 버튼 생성
-			//if(loadedNumber == pageSize) 
-				//$("#history-load-btn").removeClass("hidden");
 				
 			if($(result).find("#hasNext").text() == "true")
 				$("#history-load-btn").removeClass("hidden");
+			
+			if(callBack)
+				callBack();
 		}
 	});
 }
