7e8757aa28ca76f1a850245c6c59aa7151805051
UK <jsw4795@naver.com>
2024-10-17 16:54:46 +0900

    my-page-timeline.js 불필요 코드 삭제
    서버측 템플릿 파일로 옮기면서 필요없어진 loadTimelineBtn을 삭제하였다.

--- src/main/resources/static/js/my-page-timeline.js
+++ src/main/resources/static/js/my-page-timeline.js
@@ -143,10 +143,3 @@ function makeTimelineBlock(data) {
 	
 	return $html;
 }
-
-const loadTimelineBtn = 
-			'<div class="text-center">'
-				+'<button id="timeline-load-btn" class="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium rounded-full bg-gray-900 px-4 py-2 mt-1 text-white">'
-			        +'Load More'
-			   +'</button>'
-			+'</div>';
